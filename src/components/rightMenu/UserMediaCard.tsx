1fec96ffd4599fa03f2271443a9864c0675a3059
Ezgi Deren <ezgi_deren@yahoo.com>
2024-12-14 22:58:02 +0100

    fetch user media functionality

--- src/components/rightMenu/UserMediaCard.tsx
+++ src/components/rightMenu/UserMediaCard.tsx
@@ -1,46 +1,49 @@
-import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
-import { User } from '@prisma/client'
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { User } from "@prisma/client";
+import prisma from "@/lib/client";
 
-const UserMediaCard = ({user}: {user: User}) => {
+const UserMediaCard = async ({ user }: { user: User }) => {
+  const postsWithMedia = await prisma.post.findMany({
+    where: {
+      userId: user.id,
+      img: {
+        not: null,
+      },
+    },
+    take: 8,
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
   return (
-    <div className='p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4'>
-    {/* Title */}
-    <div className='flex justify-between items-center font-md'>
-      <span className='text-gray-500'>User Media</span>
-      <Link href="/" className="text-blue-500 text-xs">See all</Link>
+    <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4">
+      {/* Title */}
+      <div className="flex justify-between items-center font-md">
+        <span className="text-gray-500">User Media</span>
+        <Link href="/" className="text-blue-500 text-xs">
+          See all
+        </Link>
+      </div>
+      {/* Bottom */}
+      <div className="flex gap-4 justify-between flex-wrap">
+        {postsWithMedia.length
+          ? postsWithMedia.map((post) => (
+              <div className="relative w-1/5 h-24" key={post.id}>
+                <Image
+                  src={post.img!}
+                  alt=""
+                  fill
+                  className="object-cover rounded-md"
+                />
+              </div>
+            ))
+          : "No media found!"}
+      </div>
     </div>
-    {/* Bottom */}
-    <div className='flex gap-4 justify-between flex-wrap'>
-        <div className='relative w-1/5 h-24'>
-        <Image src="https://images.pexels.com/photos/29642313/pexels-photo-29642313/free-photo-of-cozy-dessert-with-chocolate-and-tea.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt='' fill className='object-cover rounded-md' />
-        </div>
-        <div className='relative w-1/5 h-24'>
-        <Image src="https://images.pexels.com/photos/29642313/pexels-photo-29642313/free-photo-of-cozy-dessert-with-chocolate-and-tea.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt='' fill className='object-cover rounded-md' />
-        </div>
-        <div className='relative w-1/5 h-24'>
-        <Image src="https://images.pexels.com/photos/29642313/pexels-photo-29642313/free-photo-of-cozy-dessert-with-chocolate-and-tea.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt='' fill className='object-cover rounded-md' />
-        </div>
-        <div className='relative w-1/5 h-24'>
-        <Image src="https://images.pexels.com/photos/29642313/pexels-photo-29642313/free-photo-of-cozy-dessert-with-chocolate-and-tea.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt='' fill className='object-cover rounded-md' />
-        </div>
-        <div className='relative w-1/5 h-24'>
-        <Image src="https://images.pexels.com/photos/29642313/pexels-photo-29642313/free-photo-of-cozy-dessert-with-chocolate-and-tea.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt='' fill className='object-cover rounded-md' />
-        </div>
-        <div className='relative w-1/5 h-24'>
-        <Image src="https://images.pexels.com/photos/29642313/pexels-photo-29642313/free-photo-of-cozy-dessert-with-chocolate-and-tea.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt='' fill className='object-cover rounded-md' />
-        </div>
-        <div className='relative w-1/5 h-24'>
-        <Image src="https://images.pexels.com/photos/29642313/pexels-photo-29642313/free-photo-of-cozy-dessert-with-chocolate-and-tea.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt='' fill className='object-cover rounded-md' />
-        </div>
-        <div className='relative w-1/5 h-24'>
-        <Image src="https://images.pexels.com/photos/29642313/pexels-photo-29642313/free-photo-of-cozy-dessert-with-chocolate-and-tea.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt='' fill className='object-cover rounded-md' />
-        </div>
-    </div>
-    
-  </div>
-  )
-}
+  );
+};
 
-export default UserMediaCard
+export default UserMediaCard;
